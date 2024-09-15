b485ae80013b3cf47d3ef3dedeca3cd24455431a
Mohamed <mohamedelyousfi87@gmail.com>
2024-09-15 19:23:23 +0200

    Add basic CLI support

--- KULpubScheduleToICS.py
+++ KULpubScheduleToICS.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """Script for exporting a KUL public SAP schedule to an ICS calender file
 Maxime Feyerick - 10/2021
 
@@ -18,28 +20,39 @@ from typing import Any, List, Tuple
 from arrow.arrow import Arrow
 import bs4
 from bs4.element import NavigableString
-import requests                         # HTML requests
-from bs4 import BeautifulSoup as Bsp    # HTML parser
-import re                               # regex matching/parsing of strings
-from ics import Calendar, Event         # creating ics exports
-import arrow                            # For time stamps
+import requests  # HTML requests
+from bs4 import BeautifulSoup as Bsp  # HTML parser
+import re  # regex matching/parsing of strings
+from ics import Calendar, Event  # creating ics exports
+import arrow  # For time stamps
+
+from argparse import ArgumentParser
 
 # user defined
-import calevent     # event data structure and conversion to ics formats
+import calevent  # event data structure and conversion to ics formats
 
 # Constants
 # ----------
 
-# URL of the semester-view schedule
-URL_SCHEDULE=r'https://webwsp.aps.kuleuven.be/sap(bD1lbiZjPTIwMA==)/public/bsp/sap/z_mijnuurrstrs/uurrooster_sem_lijst.htm?sap-params=dGl0ZWxsaWpzdD1VdXJyb29zdGVyJm90eXBlPUQmb2JqaWQ9NTEwMTE3MDkmYmVnaW53ZWVrMT0yMDI0MDkmZWluZGV3ZWVrMT0yMDI0MjAmYmVnaW53ZWVrMj0mZWluZGV3ZWVrMj0mc2Nfb2JqaWQ9MDAwMDAwMDAmc2Vzc2lvbmlkPUJEMTQwNzY3QzY3MzFFREVCMUI1RTIyOUNDNDQzMDhDJnR5cGVfZ3JvZXA9'
-#TODO: seems to be valid only for short time??
 
+parser = ArgumentParser(
+    prog='KULpubScheduleToICS',
+    description='Publishes public SAP schedule to ICS calendar',
+)
+
+parser.add_argument('source')
+parser.add_argument('-d', '--destination', default='./schedule.ics')
+
+args = parser.parse_args()
+# URL of the semester-view schedule
+URL_SCHEDULE = args.source
 
 # Time range regex (both NL and EN compatible)
 REGEXPR_TIME_RANGE = r"(\d+:\d+)\s*\w+\s*(\d+:\d+)"
 
 # Destination ics filename
-ICS_FILENAME = "./schedule.ics"
+ICS_FILENAME = args.destination
+
 
 def main():
     page = requests.get(URL_SCHEDULE)
@@ -52,7 +65,7 @@ def main():
     #   2) If different day, or first event: a row with the name of the day
     #   3) Event info, except for dates
     #   4) the dates
-    #       * This table has a second row, containing a a line seperating 
+    #       * This table has a second row, containing a line seperating
     #         the events
     # The first two are optional, depending on the weekday of the preceding event
 
@@ -62,19 +75,19 @@ def main():
     # the last element is the table with the building codes
 
     # store all found events here
-    event_list : List[calevent.CalEvent] = []
+    event_list: List[calevent.CalEvent] = []
 
     # start from first event in list of tables
     cur_ind = find_week_row(tables_lst) + 2
     stop_ind = len(tables_lst) - 1
- 
+
     while cur_ind < stop_ind:
         if is_week_row(tables_lst[cur_ind]):
             cur_ind += 2
-            
+
         event_list += process_event_rows(tables_lst, cur_ind)
         cur_ind += 2
-    
+
     # Sort events according to start time. 
     event_list.sort(key=lambda el: el.dt_start)
     # NOTE sorting is not persisted when making a calender object
@@ -82,7 +95,6 @@ def main():
     # Workaround? could modify the file: structure of ICS seems to be in blocks
 
     print_list(event_list)
-
 
     with open(ICS_FILENAME, 'w') as ics_file:
         ics_file.writelines(calevent.conv_to_ics_calendar(event_list))
@@ -95,12 +107,12 @@ def process_event_rows(table_lst: List[bs4.element.Tag], pos: int) -> List[calev
     schedule. The event starts at pos."""
 
     event_detail_row = table_lst[pos].tr.find_all('td', recursive=False)
-    event_dates_row    = table_lst[pos + 1].tr.find_all('td', recursive=False)
+    event_dates_row = table_lst[pos + 1].tr.find_all('td', recursive=False)
 
-    time_range_str  = sanitize_string(event_detail_row[1].string)
-    room_str        = sanitize_string(event_detail_row[2].string)
+    time_range_str = sanitize_string(event_detail_row[1].string)
+    room_str = sanitize_string(event_detail_row[2].string)
     course_code_str = sanitize_string(event_detail_row[3].a.string)
-    course_descr_str= sanitize_string(event_detail_row[4].string)
+    course_descr_str = sanitize_string(event_detail_row[4].string)
 
     # date strings from date row
     #date_strings_it = filter(lambda s: s is not None,[el.string for el in event_dates_row])
@@ -108,13 +120,13 @@ def process_event_rows(table_lst: List[bs4.element.Tag], pos: int) -> List[calev
 
     # parse time_range
     m = re.match(REGEXPR_TIME_RANGE, time_range_str)
-    time_start_str  = m[1]
-    time_stop_str   = m[2]
+    time_start_str = m[1]
+    time_stop_str = m[2]
 
-    return [ calevent.CalEvent(f"{course_descr_str} ({course_code_str})", room_str,
-                construct_timestamp(date, time_start_str),
-                construct_timestamp(date, time_stop_str) ) 
-            for date in date_strings_it ]
+    return [calevent.CalEvent(f"{course_descr_str} ({course_code_str})", room_str,
+                              construct_timestamp(date, time_start_str),
+                              construct_timestamp(date, time_stop_str))
+            for date in date_strings_it]
 
     # print(f""" Event found
     #     Code:\t{course_code_str}
@@ -123,6 +135,7 @@ def process_event_rows(table_lst: List[bs4.element.Tag], pos: int) -> List[calev
     #     Dates:\t{", ".join(date_strings_it)}
     #    """)
 
+
 # processing of parsed data
 # --------------------------
 
@@ -130,16 +143,18 @@ def sanitize_string(str_in: str) -> str:
     # parsed HTML contains lots of \r\n apparently
     return str_in.strip()
 
+
 def construct_timestamp(date: str, time: str) -> arrow.Arrow:
     """Convert scraped date and time into an arrow time stamp.
     date and time are in same format as in schedule."""
 
     # format of date: DD.MM 
     REGEXPR_Date = r"(\d+)\.(\d+)"
-    m = re.match(REGEXPR_Date, date) 
+    m = re.match(REGEXPR_Date, date)
     # We assume that the year is the current year. Otherwise need to scrape elsewhere on page
     date_str = f"{arrow.get().year}-{m[2].zfill(2)}-{m[1].zfill(2)}"
-    return arrow.get( date_str + " " + time).replace(tzinfo='Europe/Brussels')
+    return arrow.get(date_str + " " + time).replace(tzinfo='Europe/Brussels')
+
 
 # Aux for finding event in the table list
 # ----------------------------------------
@@ -148,27 +163,29 @@ def find_week_row(table_lst, pos: int = 0) -> int:
     """Find next row in table list containing 'week' text.
     Every new day in the semester view start with a week header"""
 
-    for i in range(pos,len(table_lst)):
+    for i in range(pos, len(table_lst)):
         if is_week_row(table_lst[i]):
             return i
-    
+
     return None
+
 
 def is_week_row(tb_el: bs4.element.Tag) -> bool:
     """Check if tb_el tag from semester view page is a week row"""
 
     if tb_el.name.lower() != 'table':
         return False
-    
+
     if tb_el.tr is None:
         return False
-    
+
     td_el = tb_el.tr.contents[0]
     if td_el.string == '\n':
-        td_el =tb_el.tr.contents[1]
+        td_el = tb_el.tr.contents[1]
     if not isinstance(td_el, str) and td_el.font is not None:
         if sanitize_string(td_el.font.string) == 'Week':
             return True
+
 
 # for debugging
 # -----------------
@@ -177,6 +194,7 @@ def print_list(lst: List[Any]):
     for el in lst:
         print(str(el) + "\t,")
     print(']')
+
 
 # this at the bottom
 if __name__ == "__main__":
